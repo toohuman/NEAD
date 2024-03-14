698b904b2a5fc5e078c7a0d498ce7c4ec736e5e2
toohuman <michael.crosscombe@proton.me>
2024-03-14 13:34:58 +0900

    Fixed the ant's observations based on the locations of other ants.

--- prettyNeatWann/domain/ant_dynamics.py
+++ prettyNeatWann/domain/ant_dynamics.py
@@ -37,6 +37,7 @@ TIMESTEP = 2./SIM_FPS       # Not sure if this will be necessary, given the fixe
 ANT_DIM = vec2d(5, 5)
 AGENT_SPEED = 10*1.75       # Taken from slimevolley, will need to adjust based on feeling
 TURN_RATE = 65 * 2 * math.pi / 360 
+VISION_RANGE = 100  # No idea what is a reasonable value for this.
 
 TRACK_TRAIL = 'all' # 'all', 'fade', 'none'
 FADE_DURATION = 15 # seconds
@@ -86,53 +87,75 @@ class Ant():
 
         # Detection scalar:
         # num of ants in cone, or distance to closes ant
-        self.V_f = 0.0
-        self.V_r = 0.0
-        self.V_b = 0.0
-        self.V_l = 0.0
+        self.V_f = None
+        self.V_r = None
+        self.V_b = None
+        self.V_l = None
+        self.vision_range = VISION_RANGE
 
-    def _detect_vision(self):
-        self.V_f = 0.0
-        self.V_r = 0.0
-        self.V_b = 0.0
-        self.V_l = 0.0
+    def _detect_vision(self, detected_ants: dict):
+        v_f = len(detected_ants['forward'])
+        v_r = len(detected_ants['right'])
+        v_b = len(detected_ants['back'])
+        v_l = len(detected_ants['left'])
 
-        return [self.V_f, self.V_r, self.V_b, self.V_l]
+        vision = [v_f, v_r, v_b, v_l]
+        denominator = np.sum([v_f, v_r, v_b, v_l])
+        if denominator != 0: vision /= denominator
+
+        self.V_f, self.V_r, self.V_b, self.V_l = vision
+
+
+    def _detect_nearby_ants(self, other_ants):
+        """
+        Detects other ants withsin a specified radius and identifies their relative
+        position quadrant based on this ant's orientation.
+
+        Parameters:
+        - other_ants (list of tuples): The (x, y) positions of other ants.
+        - radius (float): The radius within which to detect other ants.
+
+        Returns:
+        - dict: A dictionary mapping 'forward', 'right', 'back', 'left' to a list
+                of ants (represented by their positions) that are within the specified
+                radius and fall into that relative quadrant.
+        """
+        detected_ants = {'forward': [],  'left': [], 'back': [], 'right': []}
+
+        for other_ant in other_ants:
+            dx = other_ant[0] - self.pos.x
+            dy = other_ant[1] - self.pos.y
+            distance = math.sqrt(dx**2 + dy**2)
+
+            if distance <= self.vision_range:
+                # Calculate angle from self.pos to other_ant.pos, adjusting with self.theta
+                angle_to_ant = math.atan2(dy, dx)
+                # Adjusting by self.theta to align with the direction the agent is facing
+                relative_angle = angle_to_ant - self.theta
+
+                # Normalise the relative angle to be between -pi and pi
+                relative_angle = (relative_angle + math.pi) % (2 * math.pi) - math.pi
+
+                # Determine quadrant based on relative_angle
+                if -math.pi/2 < relative_angle <= math.pi/2:
+                    if 0 <= relative_angle <= math.pi/2:
+                        direction = 'forward'
+                    else:
+                        direction = 'left'
+                else:
+                    if -math.pi/2 >= relative_angle:
+                        direction = 'back'
+                    else:
+                        direction = 'right'
+
+                detected_ants[direction].append(other_ant)
+
+        return detected_ants
 
 
     def _turn(self):
         self.theta += (self.theta_dot * TIMESTEP)
         self.theta = self.theta % (2 * np.pi)
-
-
-    # def _move(self, arena):
-    #     # Move the ant in the current direction, assuming no obstacles
-    #     x = self.pos.x + self.desired_speed * math.cos(self.theta) * TIMESTEP
-    #     y = self.pos.y + self.desired_speed * math.sin(self.theta) * TIMESTEP
-        
-    #     # If leaving the cirle, adjust the angle to return back inside
-    #     if not is_rectangle_in_circle(x, y, arena[0], arena[1]):
-    #         # Calculate the angle from the center of the circle to the agent
-    #         angle_to_center = math.atan2(y - arena[0][1],x - arena[0][0])
-    #         if angle_to_center < 0:
-    #             print("Before:", angle_to_center)
-    #             angle_to_center += np.pi
-    #             print("After:", angle_to_center)
-    #         d_theta = 0
-    #         if (self.theta >= angle_to_center) and (self.theta < angle_to_center + np.pi):
-    #             d_theta = np.pi / 2 - self.desired_turn_speed
-    #         elif (self.theta < angle_to_center) or (self.theta >= angle_to_center - np.pi):
-    #             d_theta = -np.pi / 2 + self.desired_turn_speed
-                
-    #         theta = angle_to_center + d_theta
-    #         theta = theta % (2 * np.pi)
-
-    #         x = x + math.cos(theta) * TIMESTEP
-    #         y = y + math.sin(theta) * TIMESTEP
-    #         self.theta = theta
-        
-    #     # Update position based on desired velocity + arena physics
-    #     self.pos = vec2d(x, y)
 
 
     def _move(self, arena):
@@ -198,13 +221,15 @@ class Ant():
             self.desired_turn_speed = TURN_RATE
 
 
-    def get_obs(self):
+    def get_obs(self, others=None):
+        if others is not None:
+            self._detect_vision(self._detect_nearby_ants(others))
         result = [
             self.pos.x, self.pos.y, self.speed,
             self.theta, self.theta_dot,
             self.V_f, self.V_r, self.V_b, self.V_l
         ]
-        scaleFactor = 10.0
+        scaleFactor = 1.0
         return np.array(result) / scaleFactor
 
 
@@ -237,6 +262,8 @@ class AntDynamicsEnv(gym.Env):
         self.ant_trail = None
 
         self.target_trail = None
+
+        self.other_ants = None
 
         self.seed()
         self.viewer = None
@@ -300,8 +327,10 @@ class AntDynamicsEnv(gym.Env):
         # If showing positions of other ants during the trail
         other_ants = None
         if others:
-            other_ants = np.zeros((num_ants - 1, trail_length, 2),
-                                   dtype=float)
+            other_ants = np.zeros(
+                (num_ants - 1, trail_length, 2),
+                dtype=float
+            )
 
         start = np.random.randint(len(type(self).ant_trail_data) - trail_length)
         indices = list(np.random.permutation(num_ants))
@@ -341,8 +370,8 @@ class AntDynamicsEnv(gym.Env):
         return trail
 
 
-    def get_observations(self):
-        return self.ant.get_obs()
+    def get_observations(self, others=None):
+        return self.ant.get_obs(others)
 
 
     def _destroy(self):
@@ -369,7 +398,7 @@ class AntDynamicsEnv(gym.Env):
         self.ant, self.target_trail, self.other_ants = self._select_target(others=True)
         self.state = np.random.normal(loc=np.array([0.0, 0.0, np.pi, 0.0]), scale=np.array([0.2, 0.2, 0.2, 0.2]))
         x, x_dot, theta, theta_dot = self.state
-        obs = np.array([x, x_dot, np.cos(theta), np.sin(theta), theta_dot])
+        obs = self.get_observations(self.other_ants[:,self.t])
 
         if self.render_mode == 'human':
             self._render_frame()
@@ -396,8 +425,8 @@ class AntDynamicsEnv(gym.Env):
 
         self.ant.set_action(action)
         self.ant.update(self.ant_arena)
-        self.get_observations()
-
+        obs = self.get_observations(self.other_ants[:,self.t])
+        
         if self.t >= self.t_limit:
             done = True
 
@@ -438,10 +467,10 @@ class AntDynamicsEnv(gym.Env):
 
         if self.other_ants is not None:
             try:
-                for other_ant in self.other_ants:
+                for other_ant in self.other_ants[:,self.t]:
                     pygame.draw.rect(canvas, (180, 180, 180),
-                                    (other_ant[self.t][0] - ANT_DIM.x/2.,
-                                    other_ant[self.t][1] - ANT_DIM.y/2.,
+                                    (other_ant[0] - ANT_DIM.x/2.,
+                                    other_ant[1] - ANT_DIM.y/2.,
                                     ANT_DIM.x, ANT_DIM.y))
 
             except IndexError:
