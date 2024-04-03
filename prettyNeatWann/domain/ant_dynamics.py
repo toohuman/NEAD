1c0f4f6457416636ea354cb44b715e5fec73dd55
toohuman <michael.crosscombe@proton.me>
2024-04-03 15:42:03 +0900

    Moving helper functions into main file.

--- prettyNeatWann/domain/ant_dynamics.py
+++ prettyNeatWann/domain/ant_dynamics.py
@@ -2,14 +2,15 @@ import logging
 import sys, math
 from collections import namedtuple
 import numpy as np
+import pandas as pd
 import pygame
 import random
+import lzma
+import os
 
 import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.utils import colorize, seeding
-
-from data_generator import *
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -33,10 +34,10 @@ vec2d = namedtuple('vec2d', ['x', 'y'])
 # Global parameters for agent control
 TIMESTEP = 1./SIM_FPS       # Not sure if this will be necessary, given the fixed FPS?
 # TIME_LIMIT = SIM_FPS * 60   # 60 seconds
-TIME_LIMIT = SIM_FPS * 15   # 60 seconds
+TIME_LIMIT = SIM_FPS * 30   # 60 seconds
 
 ANT_DIM = vec2d(5, 5)
-AGENT_SPEED = 10*3.25       # Taken from slimevolley, will need to adjust based on feeling
+AGENT_SPEED = 15*3.25       # Taken from slimevolley, will need to adjust based on feeling
 TURN_RATE = 140 * 2 * math.pi / 360 
 VISION_RANGE = 100  # No idea what is a reasonable value for this.
 
@@ -45,7 +46,146 @@ TRACK_TRAIL = 'all' # 'all', 'fade', 'none'
 MOVEMENT_THRESHOLD = 10
 FADE_DURATION = 5 # seconds
 
-# Helper functions
+##########################################
+#          Assistance functions          #
+##########################################
+
+FILE_PREFIX = "KA050_10cm_5h_20230614"
+PP_FILE_PREFIX = "KA050_processed"
+OUTPUT_FILE = '_'.join([PP_FILE_PREFIX, *FILE_PREFIX.split('_')[1:]]) + '.pkl.xz'
+
+def load_data(source_dir, scale = None, arena_dim = None):
+    data = None
+    if os.path.exists(os.path.join(source_dir, OUTPUT_FILE)):
+        with lzma.open(os.path.join(source_dir, OUTPUT_FILE)) as file:
+            data = pd.read_pickle(file)
+            logger.info(msg=f"Processed data file found: {OUTPUT_FILE}")
+        return data.iloc[::int(scale)] if scale else data
+    else:
+        logger.info(msg=f"No processed file found. Looking for ")
+        return load_combined_files(source_dir, arena_dim, scale)
+
+
+def process_data(data, arena_dim):
+    data_len = len(data)
+    logger.info(msg=f"Ant trail data loaded. Total records: {data_len}")
+    arena_bb = find_bounding_box(data)
+    origin_arena = calculate_circle(*arena_bb)
+
+    translation, scale = circle_transformation(
+        origin_arena, arena_dim
+    )
+
+    logger.info(msg=f"Processing data now. This will take a while...")
+    apply_transform_scale(data, translation, scale)
+    logger.info(msg=f"Finished processing.")
+
+    logger.info(msg=f"Translation: {translation}, scale: {scale}")
+    logger.info(msg=f"Original: ({origin_arena[0][0] + translation[0]}, {origin_arena[0][1] + translation[1]}), scale: {origin_arena[1]*scale}")
+    logger.info(msg=f"Simulated: {arena_dim[0]}, scale: {arena_dim[1]}")
+
+    return data
+
+
+def load_combined_files(source_dir, arena_dim, scale = None):
+    input_files = []
+    data = []
+
+    for file in os.listdir(source_dir):
+        if FILE_PREFIX in file and file.endswith('.pkl.xz'):
+            input_files.append(file)
+
+    for input_file in input_files:
+        with lzma.open(os.path.join(source_dir, input_file)) as file:
+            data.append(pd.read_pickle(file))
+
+    data = process_data(pd.concat(data, ignore_index=True), arena_dim)
+    data.to_pickle(os.path.join(source_dir, OUTPUT_FILE), compression='xz')
+
+    return data.iloc[::int(scale)] if scale else data
+
+
+def find_bounding_box(data):
+    # Separate all x and y values into slices
+    all_x_values = data[[col for col in data.columns if 'x' in col]]
+    all_y_values = data[[col for col in data.columns if 'y' in col]]
+    # Calculating the minimum and maximum for x and y values efficiently
+    min_x = all_x_values.min(axis=None)
+    max_x = all_x_values.max(axis=None)
+    min_y = all_y_values.min(axis=None)
+    max_y = all_y_values.max(axis=None)
+
+    return min_x, min_y, max_x, max_y
+
+
+def calculate_circle(min_x, min_y, max_x, max_y):
+    """
+    Calculate the circle that fits perfectly within a bounding box.
+
+    Parameters:
+    min_x (float): The minimum x value of the bounding box.
+    max_x (float): The maximum x value of the bounding box.
+    min_y (float): The minimum y value of the bounding box.
+    max_y (float): The maximum y value of the bounding box.
+
+    Returns:
+    tuple: A tuple containing the center coordinates (x, y) and the radius of the circle.
+    """
+    # Calculate the center of the bounding box
+    x_centre = (min_x + max_x) / 2
+    y_centre = (min_y + max_y) / 2
+
+    # Calculate the radius of the circle
+    radius = min(max_x - min_x, max_y - min_y) / 2
+
+    return ((x_centre, y_centre), radius)
+
+
+def circle_transformation(circle_a, circle_b):
+    """
+    Calculate the transformation from one circle to another.
+
+    Parameters:
+    circle_a (tuple): A tuple (x_a, y_a, r_a) representing Circle A's center and radius.
+    circle_b (tuple): A tuple (x_b, y_b, r_b) representing Circle B's center and radius.
+
+    Returns:
+    tuple: A tuple containing the translation vector (dx, dy) and the scaling factor.
+    """
+    SF = 0.99
+    (x_a, y_a), r_a = circle_a
+    (x_b, y_b), r_b = circle_b
+
+    # Scaling factor
+    scale = r_b / r_a
+
+    x_a *= scale*SF
+    y_a *= scale*SF
+
+    # Translation vector
+    dx = x_b - x_a
+    dy = y_b - y_a
+
+    return (dx, dy), scale*SF
+
+
+def apply_transform_scale(data, trans, scale):
+    data[[col for col in data.columns if 'x' in col]] = data[[col for col in data.columns if 'x' in col]].transform(
+        np.vectorize(lambda x : np.round((x * scale) + trans[0]))
+    )
+    data[[col for col in data.columns if 'y' in col]] = data[[col for col in data.columns if 'y' in col]].transform(
+        np.vectorize(lambda x : np.round((x * scale) + trans[1]))
+    )
+
+
+def euclidean_distances(data):
+    a = np.array(data)
+    b = a.reshape(a.shape[0], 1, a.shape[1])
+    distances = np.sqrt(np.einsum('ijk, ijk->ij', a-b, a-b))
+    np.fill_diagonal(distances, np.NaN)
+
+    return distances
+
 def is_rectangle_in_circle(x, y, circle_center, circle_radius):
     """
     Check if a pygame.Rect is completely contained within a circle.
@@ -77,6 +217,10 @@ def is_rectangle_in_circle(x, y, circle_center, circle_radius):
 
     return True
 
+
+##########################################
+#            Ant Environment             #
+##########################################
 
 class Ant():
     """Agent class for the ant"""
